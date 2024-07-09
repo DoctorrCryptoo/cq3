e71e90f382ed9fc17e5ceae9a4dff3fb960d8dba
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-07-10 02:16:51 +0530

    started basic layout of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,15 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+	return (
+		<div className="flex items-center justify-between w-full m-4">
+			<img src="cnlogopotter.png" alt="logo" className="w-[6rem]" />
+			<div className="bg-[#001214] opacity-55 flex justify-around rounded-2xl h-16 w-[40%] mx-auto place-items-center">
+				<a href="" className="text-3xl text-white font-harry">About</a>
+				<a href="" className="text-3xl text-white font-harry">Prize</a>
+				<a href="" className="text-3xl text-white font-harry">Timeline</a>
+				<a href="" className="text-3xl text-white font-harry">Contact Us</a>
+			</div>
+			<div className="w-[6rem]"></div>
+		</div>
+	);
 };
 export default Navbar;
