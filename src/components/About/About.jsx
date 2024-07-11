5c194fa742de658d701ebe424a6b17a50670763e
Tanmay Bansal <tanmaybansal170@gmail.com>
2024-07-11 23:20:53 +0530

    Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,36 @@
+import Vector1 from '../../assets/Images/Vector1.png';
+import Vector2 from '../../assets/Images/Vector2.png';
+import Abouti from '../../assets/Images/About.png';
+import Book from '../../assets/Images/Book.png';
+import Train from '../../assets/Images/Train-About.png';
+import Cloudl from '../../assets/Images/cl-ab-l.png';
+import Cloudr from '../../assets/Images/cl-ab-r.png';
 const About = () => {
-  return <div>About</div>;
+  return <div>
+    <img src={Vector1} className='absolute left-[1000px] w-[700px] md:left-[815px] md:my-[60px]' />
+    <div>
+      <img src={Cloudl} className="z-30 absolute md:my-[40px] md:w-[650px] md:mx-[4px] md:h-[431px]" />
+      </div>
+      <div>
+      <img src={Cloudr} className="z-20 absolute my-[40px] md:my-[10px] mx-[50px] md:mx-[120px] md:h-[531px] md:w-[760px]" />
+      </div>
+      <div>
+      <img src={Train} className="mx-2 absolute w-[600px] my-[200px] md:my-[280px] md:w-[700px] md:h-[570px]" />
+      </div>
+      <div>
+        <img src={Abouti} className='absolute h-[60px] mx-[820px] my-[100px] md:mx-[1080px] md:my-[240px] md:w-[250px] md:h-[100px]' />
+      </div>
+      <div>
+    <p className='text-white font-sans text-center absolute text-xl mx-[700px] my-[200px] w-[400px] md:text-xl md:my-[420px] md:mx-[950px] md:w-[500px]'>Campus Quest is a team game with a combination of Treasure Hunt and Capture The Flag. The game will essentially involve the first-year students and make them explore the campus through the due course of the event. </p>
+    </div>
+    <div>
+      <img src={Book} className='absolute h-[80px] mx-[500px] my-[380px] md:mx-[900px] md:h-[160px] md:my-[600px] ' />
+      </div>
+      <div>
+      <a href="#" className='text-white absolute font-harry text-6xl mx-[530px] my-[440px] md:text-6xl md:my-[720px] md:mx-[970px]'>Rules</a>
+      </div>
+      <img src={Vector2} className='absolute left-0 my-[600px] md:w-[700px] md:my-[920px]' />
+  </div>;
 };
 export default About;
+
--- src/components/Prize/Prize.jsx
+++ src/components/Prize/Prize.jsx
@@ -1,4 +1,55 @@
+import prize from '../../assets/Images/Prize Pool.png';
+import Vector1 from '../../assets/Images/Vector1.png';
+import Vector2 from '../../assets/Images/Vector2.png';
+import Cup from '../../assets/Images/Prize.png';
+import Fire from '../../assets/Images/Fire.png';
+import Page from '../../assets/Images/Page.png';
+import Cl1 from '../../assets/Images/cl-1-P.png';
+import Cl2 from '../../assets/Images/cl-2-P.png';
+import Cl3 from '../../assets/Images/cl-3-P.png';
+import Cl4 from '../../assets/Images/cl-4-P.png';
+import Cl5 from '../../assets/Images/cl-5-P.png';
+import Cl6 from '../../assets/Images/cl-6-P.png';
 const Prize = () => {
-  return <div>Prize</div>;
+  return <div>
+        <img src={Vector1} className='absolute top-[1500px] left-[1000px] md:top-[2100px] md:left-[815px] md:w-[700px]' />
+    <div>
+    <img src={prize} className='h-[60px] absolute top-[1600px] mx-[1000px] md:h-[100px] md:top-[2200px] md:mx-[1000px]' />
+    </div>
+    <div>
+      <img src={Cup} className='absolute h-[600px] w-[400px] mx-[400px] top-[1450px] z-10 md:w-[600px] md:h-[800px] md:top-[2050px] md:mx-[200px]' />
+    </div>
+    <div>
+      <img src={Fire} className='Fire' />
+    </div>
+    <div>
+      <img src={Page} className='absolute h-[100px] mx-[1000px] my-[30px] md:h-[200px] md:mx-[1050px] md:my-[100px]' />
+    </div> 
+    <div>
+      <p className='absolute font-harry text-black text-3xl mx-[1030px] my-[50px] w-[100px] md:text-5xl md:my-[150px] md:mx-[1130px] md:w-[200px]'>Yet to be</p>
+      <p className='absolute font-harry text-red-900 text-3xl mx-[1040px] my-[85px] w-[100px] md:text-5xl md:my-[210px] md:mx-[1140px]'>Revealed</p>
+    </div>
+    <div>
+      <div>
+        <img src={Cl1} className='absolute w-[500px] my-[140px] md:my-[200px] ' />
+      </div>
+      <div>
+        <img src={Cl2} className='absolute w-[500px] my-[160px] mx-[200px] md:my-[200px] md:mx-[100px]' />
+      </div>
+      <div>
+        <img src={Cl3} className='absolute w-[500px] my-[160px] mx-[300px] md:my-[260px] md:mx-[220px]' />
+      </div>
+      <div>
+        <img src={Cl4} className='absolute w-[500px] my-[160px] mx-[400px] md:mx-[350px] md:my-[300px]' />
+      </div>
+      <div>
+       <img src={Cl5} className='absolute w-[500px] z-20 my-[180px] md:my-[170px]' />
+      </div>
+      <div>
+        <img src={Cl6} className='absolute w-[600px] z-20 my-[180px] mx-[400px] md:my-[200px] md:mx-[400px]' />
+      </div>
+    </div>
+    <img src={Vector2} className='absolute left-0 top-[2300px] md:w-[700px] md:my-[720px]' />
+    </div>;
 };
 export default Prize;
