248df2e6fafa1683227c3492b4ea1234c97d7c61
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-07-19 13:43:34 +0530

    prize fix2

--- src/components/Prize/Prize.jsx
+++ src/components/Prize/Prize.jsx
@@ -3,7 +3,7 @@ import Page from '../../assets/Images/Page.png';
 import Cup from '../../assets/Images/image (13).png';
 const Prize = () => {
   return <div className=''>
-    <img src={prize} className='mx-8' />
+    <img src={prize} className='' />
     <div className='flex flex-col-reverse xl:grid-cols-5 xl:items-center xl:grid'>
       <img src={Cup} className='self-start xl:col-span-3 max-w-screen ' />
     <img src={Page} className='self-center w-2/3 mb-48 xl:col-span-2' />
