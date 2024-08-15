4886c0d2a5405f040dd8ccd21a946711c83c13c0
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-08-16 00:55:21 +0530

    timeline fixes

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,10 +1,12 @@
 import Time from '../../assets/Images/Timeline.png';
 import steps from '../../assets/Images/steps2.png';
+import stepsMobile from '../../assets/Images/stepsmobile.png';
 
 const Timeline = () => {
   return <div className=''>
       <img src={Time} className='float-right w-2/3 pt-5 mr-5 md:pt-10 md:w-2/5' loading="lazy"/>
-      <img src={steps} loading="lazy"/>
+      <img src={steps} loading="lazy" className='hidden sm:block'/>
+      <img src={stepsMobile} loading="lazy" className='block sm:hidden'/>
 
   </div>;
 };
--- todo.md
+++ todo.md
@@ -1,10 +1,10 @@
 ### CQ Site Changes To-Do List
 
 - [ ] adding sponsors
-- [ ] timeline on mobile
+- [x] timeline on mobile
 - [x] navbar shading on scroll mobile
 - [ ] lorem ipsum string on landing
-- [ ] timeline spacing/orientation for cloud on desktop
+- [x] timeline spacing/orientation for cloud on desktop
 - [x] change hamburger icon from footsteps to 3 lines to make it more obvious
 - [ ] add rules popup (need content for it)
 - [ ] add link hrefs
