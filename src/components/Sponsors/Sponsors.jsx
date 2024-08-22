97bc151031291036a4e101f5a7662610c4275f79
Sukh Oberoi <sukhoberoi24@gmail.com>
2024-08-22 10:05:50 +0530

    Update Sponsors.jsx

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -9,25 +9,25 @@ const Sponsors = () => {
 			<img src={Sponsor} className="w-4/5 pt-5 ml-5 md:w-1/2 md:pt-10" />
 			<div className="grid grid-cols-2 gap-4 p-4 m-4 place-items-center place-content-center sm:grid-cols-4">
 				<div>
-					<img src={Red} className="h-[25dvh] lg:h-[70dvh]" loading="lazy"/>
+					<img src={Red} className="h-[25lvh] lg:h-[70dvh]" loading="lazy"/>
 					<p className="font-bluu text-[#C90909] text-xl sm:text-4xl text-center text-nowrap">
 						And that's Anime
 					</p>
 				</div>
 				<div>
-					<img src={Yellow} className="h-[25dvh] lg:h-[70dvh]" loading="lazy"/>
+					<img src={Yellow} className="h-[25lvh] lg:h-[70dvh]" loading="lazy"/>
 					<p className="font-bluu text-[#FBD702] text-xl sm:text-4xl text-center">
 						Rasoe
 					</p>
 				</div>
 				<div>
-					<img src={Blue} className="h-[25dvh] lg:h-[70dvh]" loading="lazy"/>
+					<img src={Blue} className="h-[25lvh] lg:h-[70dvh]" loading="lazy"/>
 					<p className="font-bluu text-[#00587E] text-xl sm:text-4xl text-center">
 						Interview Cake
 					</p>
 				</div>
 				<div>
-					<img src={Green} className="h-[25dvh] lg:h-[70dvh]" loading="lazy"/>
+					<img src={Green} className="h-[25lvh] lg:h-[70dvh]" loading="lazy"/>
 					<p className="font-bluu text-[#1D7239] text-xl sm:text-4xl text-center">
 						Chocoff
 					</p>
